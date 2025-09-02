91bf1ad7b32070ad488b19cb3689e9de857d1f80
0quaaD <eln6436@gmail.com>
2025-09-02 17:33:34 +0400

    Added random food spawning

--- src/snake.c
+++ src/snake.c
@@ -1,84 +1,95 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define WIDTH       790
-#define HEIGHT      790
+#define WIDTH       810
+#define HEIGHT      810
 #define CELL_SIZE   30
 #define CELL_COUNT  25
 
 Color light_green = {173, 204, 96,255}; // Background color
-Color dark_green = {43, 51, 24, 255};
+Color dark_green = {43, 51, 24, 255}; // Snake color 
 
 typedef struct {
   Vector2 pos;
-  Texture2D apple;
+  Texture2D shape;
 } Food;
 
 typedef struct {
-  int posX, posY;
-} Snake;
-
-void _Draw_FPS(char *fps) {
-  sprintf(fps, "FPS: %d", GetFPS());
-  DrawText(fps, 10, 10, 20, BLACK);
-}
-
-void drawBorder(int limX, int limY,Color color) {
-  DrawLine(limX, limY, WIDTH-limX, limY, color); // Up line
-  DrawLine(limX, limY, limX, HEIGHT - limY, color); // Left line 
-  DrawLine(limX, HEIGHT - limY, WIDTH-limX, HEIGHT-limY, color); // Down line 
-  DrawLine(WIDTH-limX, limY, WIDTH-limX, HEIGHT-limY, color); // Right line
-}
+  int _limX, _limY;   // Top left
+  int limX_, limY_;   // Top right
+  int __limX, __limY; // Bottom left
+  int limX__, limY__; //Bottom right
+} Border;
 
 void drawFood(Food *food) {
-  DrawTextureEx(food->apple, food->pos, 0.0f, (float)CELL_SIZE / food->apple.width, light_green);
+  Rectangle source = {0, 0, food->shape.width, food->shape.height};
+  Rectangle dest = {food->pos.x, food->pos.y, CELL_SIZE, CELL_SIZE};
+  Vector2 origin = {0, 0};
+  DrawTexturePro(food->shape, source, dest, origin, 0.0f, light_green);
+}
+
+Vector2 getRandomFood() {
+  int randX = (rand() % (CELL_COUNT - 1));
+  int randY = (rand() % (CELL_COUNT - 1));
+
+  Vector2 pos = {randX * CELL_SIZE + 30, randY * CELL_SIZE + 40};
+  return pos;
+}
+
+void drawFps(char *fps) {
+  sprintf(fps, "FPS: %d", GetFPS());
+  DrawText(fps, 10, 10, 20, BLACK);
 }
 
 int get_score() {
   return 0;  
 }
 
+void drawBorder(Border *border, Color color) {
+  DrawLine(border->_limX, border->_limY, border->limX_, border->limY_, color);
+  DrawLine(border->__limX , border->__limY,border->limX__, border->limY__, color);
+  DrawLine(border->_limX, border->_limY, border->__limX, border->__limY, color);
+  DrawLine(border->limX_, border->limY_, border->limX__, border->limY__, color);  
+}
+
 void drawScore(char *score) {
   sprintf(score, "Score: %d", get_score());
-  DrawText(score, 690, 10, 20, BLACK);
+  DrawText(score, 710, 10, 20, BLACK);
 }
-
-Vector2 generate_rand_value(int limX, int limY) {
-  float randX = CELL_SIZE * GetRandomValue(0, CELL_SIZE - 1);
-  float randY = CELL_SIZE * GetRandomValue(0, CELL_SIZE - 1);
-  return (Vector2){randX, randY};
-}
-
 
 int main(void) {
-
-  InitWindow(WIDTH, HEIGHT, "Window");
+  srand(time(NULL));
+  InitWindow(WIDTH, HEIGHT, "The Snake Game");
   SetTargetFPS(60);
 
-  int limX = 40, limY = 40;
   Texture2D apple = LoadTexture("./src/assets/apple.jpg");
+  Food food = {getRandomFood(), apple};
+  Border border = {
+      30,         40,          // Top left
+      WIDTH - 30, 40,          // Top right
+      30,         HEIGHT - 40, // Bottom left
+      WIDTH - 30, HEIGHT - 40  // Bottom right
+  };
   
-  Food food = { generate_rand_value(limX, limY), apple};
-  Snake snake = {};
   while (!WindowShouldClose()) {
-    // --- Update part ---
-    
-    // --- Draw part ---
     BeginDrawing();
     ClearBackground(light_green);
-
-    drawBorder(limX, limY, BLACK);
-    drawFood(&food);
-    
-    // Get Score 
+    drawBorder(&border, BLACK);
     char score[32];
     drawScore(score);
-    
-    // Draw FPS on game screen
-    char fpsText[32];
-    _Draw_FPS(fpsText);
+
+    drawFood(&food);
+ 
+    //Draw fps value    
+    char fps[32];
+    drawFps(fps);
     EndDrawing();
   }
+
+  // Free the memory before return 0
+  UnloadTexture(apple); 
   CloseWindow();
   return 0;  
 }
